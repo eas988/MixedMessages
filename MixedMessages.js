d5d5ba3326ea9e6b296d599b3d7c33e7956827ee
Evan Small <eas988@gmail.com>
2021-06-10 17:14:21 -0400

    Revised GWT of project

--- MixedMessages.js
+++ MixedMessages.js
@@ -2,7 +2,7 @@
 //I want the song-generator to create multiple random batches of three or four syllable per line lyrics and a set chord progression in a random key
 //So that I can immediately play a prefabricated song
 
-//Given the song-generator is run
-//When the song-generator is set to four batches and the three syllable per line lyric option
+//Given the song-generator is set to four batches and the three syllable per line lyric option
+//When the song-generator is run
 //Then a random string of lyrics a total of twelve syllables long (four batches of three syllables), a chord progression, and a random musical key are generated, ex: [["It's alright", "to tell me", "what you think", "about me"], "I–V–vi–IV", "C major"]
 
